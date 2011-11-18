ab02ae479c422b1d88552fe6b3ce83c0ed9eb7e2
Kolin Krewinkel <kolin.krewinkel@me.com>
2011-11-17 22:58:23 -0800

    Initial Commit

---
+++ KKCoreTextUtils/KKCoreTextUtils.h
@@ -0,0 +1,13 @@
+//
+//  KKCoreTextUtils.h
+//  KKCoreTextUtils
+//
+//  Created by Kolin Krewinkel on 11.17.11.
+//  Copyright (c) 2011 Enkla. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KKCoreTextUtils : NSObject
+
+@end
---
+++ KKCoreTextUtils/KKCoreTextUtils.m
@@ -0,0 +1,23 @@
+//
+//  KKCoreTextUtils.m
+//  KKCoreTextUtils
+//
+//  Created by Kolin Krewinkel on 11.17.11.
+//  Copyright (c) 2011 Enkla. All rights reserved.
+//
+
+#import "KKCoreTextUtils.h"
+
+@implementation KKCoreTextUtils
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
